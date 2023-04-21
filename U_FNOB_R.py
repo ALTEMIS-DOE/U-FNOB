50eaf56866b71a54afe001d985608ec0190e1cae
Aurelien Meray <aurelien.meray@gmail.com>
2023-04-21 14:32:08 -0400

    Add files via upload

---
+++ U_FNOB_R.py
@@ -0,0 +1,293 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import operator
+from functools import reduce
+from functools import partial
+
+torch.manual_seed(0)
+
+################################################################
+# fourier layer
+# ###############################################################
+
+class SpectralConv2d_fast(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super(SpectralConv2d_fast, self).__init__()
+
+        """
+        2D Fourier layer. It does FFT, linear transform, and Inverse FFT.    
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1 #Number of Fourier modes to multiply, at most floor(N/2) + 1
+        self.modes2 = modes2
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+        self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul2d(self, input, weights):
+        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # Compute Fourier coeffcients up to factor of e^(- something constant)
+        x_ft = torch.fft.rfft2(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+        out_ft[:, :, -self.modes1:, :self.modes2] = \
+            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+
+        # Return to physical space
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        return x
+
+    
+class U_net(nn.Module):
+    def __init__(self, input_channels, output_channels, kernel_size, dropout_rate):
+        super(U_net, self).__init__()
+        self.input_channels = input_channels
+        self.conv1 = self.conv(input_channels, output_channels, kernel_size=kernel_size, stride=2, dropout_rate = dropout_rate)
+        self.conv2 = self.conv(input_channels, output_channels, kernel_size=kernel_size, stride=2, dropout_rate = dropout_rate)
+        self.conv2_1 = self.conv(input_channels, output_channels, kernel_size=kernel_size, stride=1, dropout_rate = dropout_rate)
+        self.conv3 = self.conv(input_channels, output_channels, kernel_size=kernel_size, stride=2, dropout_rate = dropout_rate)
+        self.conv3_1 = self.conv(input_channels, output_channels, kernel_size=kernel_size, stride=1, dropout_rate = dropout_rate)
+        
+        self.deconv2 = self.deconv(input_channels, output_channels)
+        self.deconv1 = self.deconv(input_channels*2, output_channels)
+        self.deconv0 = self.deconv(input_channels*2, output_channels)
+    
+        self.output_layer = self.output(input_channels*2, output_channels, 
+                                         kernel_size=kernel_size, stride=1, dropout_rate = dropout_rate)
+
+
+    def forward(self, x):
+        out_conv1 = self.conv1(x)
+        out_conv2 = self.conv2_1(self.conv2(out_conv1))
+        out_conv3 = self.conv3_1(self.conv3(out_conv2))
+        out_deconv2 = self.deconv2(out_conv3)
+        concat2 = torch.cat((out_conv2, out_deconv2), 1)
+        out_deconv1 = self.deconv1(concat2)
+        concat1 = torch.cat((out_conv1, out_deconv1), 1)
+        out_deconv0 = self.deconv0(concat1)
+        concat0 = torch.cat((x, out_deconv0), 1)
+        out = self.output_layer(concat0)
+
+        return out
+
+    def conv(self, in_planes, output_channels, kernel_size, stride, dropout_rate):
+        return nn.Sequential(
+            nn.Conv2d(in_planes, output_channels, kernel_size=kernel_size,
+                      stride=stride, padding=(kernel_size - 1) // 2, bias = False),
+            nn.BatchNorm2d(output_channels),
+            nn.LeakyReLU(0.1, inplace=True),
+            nn.Dropout(dropout_rate)
+        )
+
+    def deconv(self, input_channels, output_channels):
+        return nn.Sequential(
+            nn.ConvTranspose2d(input_channels, output_channels, kernel_size=4,
+                               stride=2, padding=1),
+            nn.LeakyReLU(0.1, inplace=True)
+        )
+
+    def output(self, input_channels, output_channels, kernel_size, stride, dropout_rate):
+        return nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size,
+                         stride=stride, padding=(kernel_size - 1) // 2)
+    
+    
+
+class FNO2d(nn.Module):
+    def __init__(self, modes1, modes2, width):
+        super(FNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, c=12)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.padding = 2 # pad the domain if input is non-periodic
+        self.fc0 = nn.Linear(15, self.width)
+        # input channel is 15: input_array 11 + output_array 4 (last timestep)
+
+        self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.w1 = nn.Conv2d(self.width, self.width, 1)
+        self.w2 = nn.Conv2d(self.width, self.width, 1)
+        self.w3 = nn.Conv2d(self.width, self.width, 1)
+        self.bn0 = torch.nn.BatchNorm2d(self.width)
+        self.bn1 = torch.nn.BatchNorm2d(self.width)
+        self.bn2 = torch.nn.BatchNorm2d(self.width)
+        self.bn3 = torch.nn.BatchNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 4)
+
+    def forward(self, x):
+        #grid = self.get_grid(x.shape, x.device)
+        #x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+        # x = F.pad(x, [0,self.padding, 0,self.padding]) # pad the domain if input is non-periodic
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv1(x)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv2(x)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv3(x)
+        x2 = self.w3(x)
+        x = x1 + x2
+
+        # x = x[..., :-self.padding, :-self.padding] # pad the domain if input is non-periodic
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
+
+    
+class UFNO2d(nn.Module):
+    def __init__(self, modes1, modes2, width, UNet=True):
+        super(UFNO2d, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, c=12)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, c=1)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.UNet = UNet
+        
+        self.padding = 2 # pad the domain if input is non-periodic
+        self.fc0 = nn.Linear(15, self.width)
+        # input channel is 15: input_array 11 + output_array 4 (last timestep)
+
+        self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv4 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv5 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        
+        self.w0 = nn.Conv2d(self.width, self.width, 1)
+        self.w1 = nn.Conv2d(self.width, self.width, 1)
+        self.w2 = nn.Conv2d(self.width, self.width, 1)
+        self.w3 = nn.Conv2d(self.width, self.width, 1)
+        self.w4 = nn.Conv2d(self.width, self.width, 1)
+        self.w5 = nn.Conv2d(self.width, self.width, 1)
+                
+        self.unet3 = U_net(self.width, self.width, 3, 0)
+        self.unet4 = U_net(self.width, self.width, 3, 0)
+        self.unet5 = U_net(self.width, self.width, 3, 0)
+        
+        self.bn0 = torch.nn.BatchNorm2d(self.width)
+        self.bn1 = torch.nn.BatchNorm2d(self.width)
+        self.bn2 = torch.nn.BatchNorm2d(self.width)
+        self.bn3 = torch.nn.BatchNorm2d(self.width)
+        self.bn4 = torch.nn.BatchNorm2d(self.width)
+        self.bn5 = torch.nn.BatchNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, 4)
+
+    def forward(self, x):
+        #grid = self.get_grid(x.shape, x.device)
+        #x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+        padding = [8,7]
+        x = F.pad(x, [0,padding[1], 0,padding[0]]) # pad the domain if input is non-periodic
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.relu(x)
+
+        x1 = self.conv1(x)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.relu(x)
+        
+        x1 = self.conv2(x)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.relu(x)
+
+        x1 = self.conv3(x)
+        x2 = self.w3(x)
+        x3 = self.unet3(x) 
+        
+        if self.UNet: 
+            x = x1 + x2 + x3
+        else:
+            x = x1 + x2
+        x = F.relu(x)
+
+        x1 = self.conv4(x)
+        x2 = self.w4(x)
+        x3 = self.unet4(x) 
+        
+        if self.UNet: 
+            x = x1 + x2 + x3
+        else:
+            x = x1 + x2
+        
+        x1 = self.conv5(x)
+        x2 = self.w5(x)
+        x3 = self.unet5(x) 
+        
+        if self.UNet: 
+            x = x1 + x2 + x3
+        else:
+            x = x1 + x2
+            
+        x = x[..., :-padding[0], :-padding[1]] # pad the domain if input is non-periodic
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+    
